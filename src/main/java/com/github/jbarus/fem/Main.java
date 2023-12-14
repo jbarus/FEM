f4f6d22e7fb7d27c44312ec5698652f19d6614dd
jbarus <57799873+jbarus@users.noreply.github.com>
2023-12-14 15:40:48 +0100

    Initial commit

---
+++ src/main/java/com/github/jbarus/fem/Main.java
@@ -0,0 +1,15 @@
+package com.github.jbarus.fem;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+
+    }
+}
