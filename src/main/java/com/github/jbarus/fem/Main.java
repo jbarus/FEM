ae1296b047fc68970ffc502c6f41daad3e8103dd
jbarus <57799873+jbarus@users.noreply.github.com>
2023-12-19 20:17:22 +0100

    Added code for vector P and matrix H agregation calculation

--- src/main/java/com/github/jbarus/fem/Main.java
+++ src/main/java/com/github/jbarus/fem/Main.java
@@ -5,6 +5,7 @@ import com.github.jbarus.fem.global.GlobalData;
 import com.github.jbarus.fem.global.UniversalElement;
 import com.github.jbarus.fem.integration.MatrixH;
 import com.github.jbarus.fem.integration.MatrixHBCandP;
+import com.github.jbarus.fem.integration.SystemOfEquations;
 import com.github.jbarus.fem.structures.Element;
 import com.github.jbarus.fem.structures.Grid;
 import com.github.jbarus.fem.structures.Node;
@@ -22,17 +23,17 @@ public class Main {
         dataLoader.loadGrid(grid);
         UniversalElement universalElement = new UniversalElement(2);
         MatrixH matrixH = new MatrixH(universalElement,globalData);
-        MatrixHBCandP matrixHBC = new MatrixHBCandP(universalElement,globalData);
+        MatrixHBCandP matrixHBCandP = new MatrixHBCandP(universalElement,globalData);
+        SystemOfEquations systemOfEquations = new SystemOfEquations();
         for(Element element : grid.getElements()){
             Node[] nodes = new Node[4];
             for (int i = 0; i < 4; i++) {
                 nodes[i] = grid.getNodes()[element.getId()[i]-1];
             }
             matrixH.calculateHandC(element,nodes);
-            matrixHBC.calculateHBC(element,nodes);
-            System.out.println(Arrays.deepToString(element.getP()));
-
+            matrixHBCandP.calculateHBC(element,nodes);
+            //System.out.println(Arrays.deepToString(element.getP()));
         }
-
+        systemOfEquations.calculateGlobal(grid);
     }
 }
---
+++ src/main/java/com/github/jbarus/fem/integration/SystemOfEquations.java
@@ -0,0 +1,25 @@
+package com.github.jbarus.fem.integration;
+
+import com.github.jbarus.fem.structures.Element;
+import com.github.jbarus.fem.structures.Grid;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import java.util.Arrays;
+
+public class SystemOfEquations {
+    public void calculateGlobal(Grid grid){
+        RealMatrix HGlobal = MatrixUtils.createRealMatrix(grid.getNodes().length,grid.getNodes().length);
+        RealMatrix PGlobal = MatrixUtils.createRealMatrix(16,1);
+        for(Element element : grid.getElements()){
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    HGlobal.addToEntry(element.getId()[i]-1,element.getId()[j]-1,element.getH()[i][j]+element.getHBC()[i][j]);
+                    //CGlobal.addToEntry(element.getID()[i]-1,element.getID()[j]-1,element.getC()[i][j]);
+                }
+                PGlobal.addToEntry(element.getId()[i]-1,0,element.getP()[i][0]);
+            }
+        }
+        System.out.println(Arrays.deepToString(PGlobal.getData()));
+    }
+}
